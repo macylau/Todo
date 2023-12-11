dde36f86b7442fab937b2b3b00f7e5227ce5f143
Macy Lau <macylau94@gmail.com>
2023-12-10 22:36:09 -0700

    prettier format

--- client/src/main.js
+++ client/src/main.js
@@ -22,14 +22,14 @@ let todos = [
 
 let categories = ["General", "School", "Work"];
 
-let baseURL = 'http://localhost:3000'
+let baseURL = "http://localhost:3000";
 
 // Async function to fetch data
 async function fetchData(endpoint, method, data = null) {
   const options = {
     method,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: data ? JSON.stringify(data) : null,
   };
@@ -42,7 +42,7 @@ async function fetchData(endpoint, method, data = null) {
 
     return await response.json();
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
     throw error;
   }
 }
@@ -126,7 +126,10 @@ function populateCategories() {
 
 // Add a new todo
 async function addTodo(todoText, selectedCategory) {
-  const category = typeof selectedCategory === 'string' ? selectedCategory : selectedCategory.categoryName;
+  const category =
+    typeof selectedCategory === "string"
+      ? selectedCategory
+      : selectedCategory.categoryName;
   const newTodo = {
     todoText: todoText,
     todoComplete: false,
@@ -134,14 +137,14 @@ async function addTodo(todoText, selectedCategory) {
   };
 
   try {
-    const createdTodo = await fetchData('/api/todo', 'POST', newTodo);
+    const createdTodo = await fetchData("/api/todo", "POST", newTodo);
     // Add the new todo to the local todos array
     todos.push(createdTodo);
     // Update the UI or perform other actions as needed
     addTodoToDom(createdTodo);
     updatePendingTasks();
   } catch (error) {
-    console.error('Error adding todo:', error);
+    console.error("Error adding todo:", error);
   }
 }
 
@@ -159,10 +162,12 @@ async function toggleComplete(todoID) {
     displayTodosByCategory(categorySelect.value);
 
     try {
-      const response = await fetchData(`/api/todo/${todoID}`, 'PUT', { todoComplete: todos[todoIndex].todoComplete });
+      const response = await fetchData(`/api/todo/${todoID}`, "PUT", {
+        todoComplete: todos[todoIndex].todoComplete,
+      });
       console.log(response);
     } catch (error) {
-      console.error('Error updating todo:', error);
+      console.error("Error updating todo:", error);
     }
   }
 }
@@ -179,10 +184,10 @@ async function deleteTodo(todoID) {
     displayTodosByCategory(categorySelect.value);
 
     try {
-      const response = await fetchData(`/api/todo/${todoID}`, 'DELETE');
+      const response = await fetchData(`/api/todo/${todoID}`, "DELETE");
       console.log(response);
     } catch (error) {
-      console.error('Error deleting todo:', error);
+      console.error("Error deleting todo:", error);
     }
   }
 }
@@ -201,19 +206,21 @@ function clearDone() {
   todos = todos.filter((todo) => !todo.todoComplete);
 
   // Remove completed todos from the UI
-  const completedLiElements = Array.from(todoList.getElementsByClassName("done"));
+  const completedLiElements = Array.from(
+    todoList.getElementsByClassName("done")
+  );
   completedLiElements.forEach((li) => todoList.removeChild(li));
 
   // Update pending tasks
   updatePendingTasks();
 
   // Make the fetch call to clear completed todos on the server
-  fetchData(`/api/todo/${todoID}`, 'DELETE')
-    .then(response => {
+  fetchData(`/api/todo/${todoID}`, "DELETE")
+    .then((response) => {
       // Handle the response if needed
       console.log(response);
     })
-    .catch(error => console.error('Error clearing completed todos:', error));
+    .catch((error) => console.error("Error clearing completed todos:", error));
 }
 
 // Add new todo with Enter key
@@ -239,102 +246,122 @@ addButton.addEventListener("click", () => {
 clearButton.addEventListener("click", clearDone);
 
 // Add new Category
-document.querySelector("#addCategoryButton").addEventListener("click", async () => {
-  const newCategory = prompt("Enter new category:");
+document
+  .querySelector("#addCategoryButton")
+  .addEventListener("click", async () => {
+    const newCategory = prompt("Enter new category:");
 
-  if (newCategory) {
-    const existingCategory = categories.find(category => category === newCategory);
+    if (newCategory) {
+      const existingCategory = categories.find(
+        (category) => category === newCategory
+      );
 
-    if (!existingCategory) {
-      try {
-        const createdCategory = await fetchData('/api/category', 'POST', { categoryName: newCategory });
-        // Add the new category to the client-side array (only the category name)
-        categories.push(createdCategory.categoryName);
-        // Sort and update categories in the dropdown
-        sortCategories();
-      } catch (error) {
-        console.error('Error adding category:', error);
+      if (!existingCategory) {
+        try {
+          const createdCategory = await fetchData("/api/category", "POST", {
+            categoryName: newCategory,
+          });
+          // Add the new category to the client-side array (only the category name)
+          categories.push(createdCategory.categoryName);
+          // Sort and update categories in the dropdown
+          sortCategories();
+        } catch (error) {
+          console.error("Error adding category:", error);
+        }
+      } else {
+        alert("Category already exists!");
       }
-    } else {
-      alert("Category already exists!");
     }
-  }
-});
-
+  });
 
 // Edit Category
-document.querySelector("#editCategoryButton").addEventListener("click", async () => {
-  let selectedCategory = categorySelect.value;
-  let editedCategory = prompt("Enter the new category name:", selectedCategory);
+document
+  .querySelector("#editCategoryButton")
+  .addEventListener("click", async () => {
+    let selectedCategory = categorySelect.value;
+    let editedCategory = prompt(
+      "Enter the new category name:",
+      selectedCategory
+    );
 
-  if (editedCategory && editedCategory !== selectedCategory) {
-    try {
-      // Get the category ID for the selected category
-      const categoryId = categories.findIndex(category => category === selectedCategory);
+    if (editedCategory && editedCategory !== selectedCategory) {
+      try {
+        // Get the category ID for the selected category
+        const categoryId = categories.findIndex(
+          (category) => category === selectedCategory
+        );
 
-      // Make the fetch call to update the category on the server
-      const response = await fetchData(`/api/category/${categoryId}`, 'PUT', { categoryName: editedCategory });
+        // Make the fetch call to update the category on the server
+        const response = await fetchData(`/api/category/${categoryId}`, "PUT", {
+          categoryName: editedCategory,
+        });
 
-      // Assuming the server responds with the updated category
-      const updatedCategory = response;
+        // Assuming the server responds with the updated category
+        const updatedCategory = response;
 
-      // Update the categories array
-      categories[categoryId] = updatedCategory.categoryName;
+        // Update the categories array
+        categories[categoryId] = updatedCategory.categoryName;
 
-      // Sort and update categories in the dropdown
-      sortCategories();
+        // Sort and update categories in the dropdown
+        sortCategories();
 
-      // Update todos with the new category name
-      todos.forEach((todo) => {
-        if (todo.category === selectedCategory) {
-          todo.category = updatedCategory.categoryName;
-        }
-      });
+        // Update todos with the new category name
+        todos.forEach((todo) => {
+          if (todo.category === selectedCategory) {
+            todo.category = updatedCategory.categoryName;
+          }
+        });
 
-      // Display todos for the edited category
-      displayTodosByCategory(updatedCategory.categoryName);
-    } catch (error) {
-      console.error('Error updating category:', error);
+        // Display todos for the edited category
+        displayTodosByCategory(updatedCategory.categoryName);
+      } catch (error) {
+        console.error("Error updating category:", error);
+      }
     }
-  }
-});
+  });
 
 // Delete Category
-document.querySelector("#deleteCategoryButton").addEventListener("click", async () => {
-  let selectedCategory = categorySelect.value;
+document
+  .querySelector("#deleteCategoryButton")
+  .addEventListener("click", async () => {
+    let selectedCategory = categorySelect.value;
 
-  if (selectedCategory !== "All") {
-    try {
-      // Get the category ID for the selected category
-      const selectedCategoryId = categories.findIndex(category => category === selectedCategory);
+    if (selectedCategory !== "All") {
+      try {
+        // Get the category ID for the selected category
+        const selectedCategoryId = categories.findIndex(
+          (category) => category === selectedCategory
+        );
 
-      // Make the fetch call to delete the category on the server
-      const response = await fetchData(`/api/category/${selectedCategoryId}`, 'DELETE');
-      console.log(response); // Log the response if needed
+        // Make the fetch call to delete the category on the server
+        const response = await fetchData(
+          `/api/category/${selectedCategoryId}`,
+          "DELETE"
+        );
+        console.log(response); // Log the response if needed
 
-      // Remove the category from the categories array
-      categories.splice(selectedCategoryId, 1);
+        // Remove the category from the categories array
+        categories.splice(selectedCategoryId, 1);
 
-      // Sort and update categories in the dropdown
-      sortCategories();
+        // Sort and update categories in the dropdown
+        sortCategories();
 
-      // Update todos with the "All" category for deleted category items
-      todos.forEach((todo) => {
-        if (todo.category === selectedCategory) {
-          todo.category = "All";
-        }
-      });
+        // Update todos with the "All" category for deleted category items
+        todos.forEach((todo) => {
+          if (todo.category === selectedCategory) {
+            todo.category = "All";
+          }
+        });
 
-      // Display todos for the "All" category
-      displayTodosByCategory("All");
-    } catch (error) {
-      console.error('Error deleting category:', error);
+        // Display todos for the "All" category
+        displayTodosByCategory("All");
+      } catch (error) {
+        console.error("Error deleting category:", error);
+      }
     }
-  }
-});
+  });
 
-
-  categorySelect.addEventListener("change", () => {
+categorySelect.addEventListener("change", () => {
   displayTodosByCategory(categorySelect.value);
 });
 
@@ -345,7 +372,7 @@ async function initializePage() {
     categories = fetchedCategories;
     initializeTodos();
   } catch (error) {
-    console.error('Error initializing page:', error);
+    console.error("Error initializing page:", error);
   }
 }
 
